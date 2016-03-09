f71b5e8352c4970c9fecfa85e539aff410a11f45
Michael Sarahan <msarahan@gmail.com>
2016-03-09 12:13:28 -0600

    add TDD for sentiment analysis and data objects to guide development

---
+++ analyzers/tests/test_data_objects.py
@@ -0,0 +1,25 @@
+from ..data_objects import DataSource
+
+
+def test_select_data():
+    pass
+
+
+def test_select_data_with_subdata():
+    pass
+
+
+def test_select_nested_data():
+    pass
+
+
+def test_append_data():
+    pass
+
+
+def test_append_data_subdata():
+    pass
+
+
+def test_append_nested_data():
+    pass
