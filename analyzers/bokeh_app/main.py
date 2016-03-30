flesh out bokeh app; add end_time to division; fix plotting normalization

analyzers/bokeh_app/main.py
@@ -1,46 +1,83 @@
-import numpy as np
-
-from bokeh.models import Button, TextInput, DateRangeSlider, Dropdown
-from bokeh.plotting import curdoc, vplot
+from bokeh.models import TextInput, DateRangeSlider, Dropdown
+from bokeh.plotting import curdoc, vplot, hplot
 import sentiment_history_plot
 
+import elasticsearch
 
-# add a text renderer to out plot (no data yet)
-r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
-           text_baseline="middle", text_align="center")
+initial_data = sentiment_history_plot.query_sentiment()
+plot, renderers = sentiment_history_plot.plot_data(initial_data)
 
-i = 0
-renderers, plot = sentiment_history_plot.
+# UI elements
+hosts_input = TextInput(title="Elasticsearch host")
+index_input = Dropdown(title="Elasticsearch index")
+doctype_input = Dropdown(title="Elasticsearch doctype")
+query_input = TextInput(title="Elasticsearch query")
+content_field_input = Dropdown(title="Content field to analyze")
+time_field_input = Dropdown(title="Field containing date information")
+interval_value_input = TextInput(title="Time interval")
+interval_units_input = Dropdown(default_value="days", menu=[("days", "d"), ("hours", "h")])
+date_range_input = DateRangeSlider(title="Date range")
 
-ds = r.data_source
 
-# create a callback that will add a number in a random location
-def callback():
-    global i
-    ds.data['x'].append(np.random.random()*70 + 15)
-    ds.data['y'].append(np.random.random()*70 + 15)
-    ds.data['text'].append(str(i))
+# with new server specified, populate the available indices
+def update_server_info():
+    instance = elasticsearch.Elasticsearch(hosts_input.value)
+    index_input.menu = instance.cat.indices(h="index").split()
+    # TODO: if only one present, select automatically
+
+
+# with index specified, populate doctype
+def update_index():
+    instance = elasticsearch.Elasticsearch(hosts_input.value)
+    doctype_input.menu = instance.indices.get_mapping(index_input.value)[index_input.value]["mappings"].keys()
+    # TODO: if only one present, select automatically
+
+
+# with index and doctype specified, populate the available fields
+def update_doctype_info():
+    instance = elasticsearch.Elasticsearch(hosts_input.value)
+    mapping = instance.indices.get_mapping(index_input.value)[index_input.value]["mappings"][doctype_input.value]
+    content_field_input.menu = [field for field in mapping["properties"] if mapping["properties"][field]["type"] =='string']
+    time_field_input.menu = [field for field in mapping["properties"] if mapping["properties"][field]["type"] == 'date']
+    # TODO: if both content and time have only one possibility, choose automatically
+
+
+# data update callback
+def update_data():
+    interval = interval_value_input.value + interval_units_input.value
+    start_date, end_date = date_range_input.value
+    new_data = sentiment_history_plot.query_sentiment(content_field=content_field_input.value,
+                                                      time_field=time_field_input.value,
+                                                      hosts=hosts_input.value,
+                                                      query=query_input.value,
+                                                      doctype=doctype_input.value,
+                                                      start_time=start_date,
+                                                      end_time=end_date,
+                                                      interval=interval,
+                                                      )
+    area_renderers = renderers["area"]
+    areas, time = sentiment_history_plot.data_to_areas(new_data["area"])
+    for category in area_renderers:
+        ds = area_renderers[category.data_source]
+        ds.data['x'] = time
+        ds.data['y'] = areas[category]
+        ds.trigger('data', ds.data, ds.data)
+    ds = renderers["mean"].data_source
+    ds["x"] = new_data["mean"].index
+    ds["y"] = new_data["mean"].values
     ds.trigger('data', ds.data, ds.data)
-    i = i + 1
 
-def hosts_changed():
-    pass
 
-def query_changed():
-    pass
-
-def date_changed():
-    pass
-
-def time_group_changed():
-    pass
-
-# add a button widget and configure with the call back
-hosts = TextInput(title="Elasticsearch host", callback=hosts_changed)
-query = TextInput(title="Elasticsearch query", callback=hosts_changed)
-interval_value = TextInput(title="Time interval", callback=hosts_changed, callback=)
-interval_units = Dropdown(default_value="days", menu=[("days", "d"), ("hours", "h")], callback=)
-date_range = DateRangeSlider(title="Date range", callback=)
+# set up widgets with callback:
+hosts_input.on_change('value', update_server_info)
+index_input.on_change('value', update_index)
+doctype_input.on_change('value', update_doctype_info)
+for w in [content_field_input, time_field_input, interval_units_input, interval_value_input, date_range_input]:
+    w.on_change('value', update_data)
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(vplot(hosts, query, interval_value, interval_units, date_range, p))
+curdoc().add_root(vplot(
+    hplot(hosts_input, index_input, content_field_input, time_field_input),
+    hplot(query_input, interval_value_input, interval_units_input, date_range_input),
+    plot)
+)

analyzers/bokeh_app/sentiment_history_plot.py
@@ -70,12 +70,14 @@ def get_sentiment_dataframe(elasticsearch_iterable, content_field, time_field):
     return df
 
 
-def slice_data(data, start_time, interval):
+def slice_data(data, start_time, end_time, interval):
     """Divide input data up into time chunks.
 
     data : a Pandas dataframe with the time as the primary index
     start_time : A datetime.datetime object representing a time to start from.  Times after this time are not part
          of the output of this function.  A value of None means datetime.datetime.now()
+    end_time : the furthest back in time that we should look at.
+         A value of None means go to the end of the data.
     interval : A datetime.timedelta object.  Times start from the start_time, and step backwards through time with
          this timedelta as the step size.  This is the time-size of each chunk.
     """
@@ -85,7 +87,7 @@ def slice_data(data, start_time, interval):
     if not start_time:
         current_time = datetime.datetime.now()
     intervals = {}
-    min_time = min(data.index)
+    min_time = end_time if end_time else min(data.index)
     while current_time > min_time:
         start = data.index.searchsorted(current_time - interval)
         end = data.index.searchsorted(current_time)
@@ -118,12 +120,12 @@ def time_groups_to_plot_elements(slices, nbins, content_field):
 
 
 def query_sentiment(content_field, time_field, query={}, hosts="https://localhost:9200", doctype="comment",
-                    start_time=None, interval=datetime.timedelta(days=1), categories=["negative", "null", "positive"]):
+                    start_time=None, end_time=None, interval=datetime.timedelta(days=1), categories=["negative", "null", "positive"]):
     records = get_elasticsearch_data(hosts=hosts, content_field=content_field, time_field=time_field, query=query,
-                                     doc_type=doctype)
+                                     doc_type=doctype, start_time=start_time, end_time=end_time)
     # takes iterable records, outputs pandas dataframe
     raw_df = get_sentiment_dataframe(records, content_field=content_field, time_field=time_field)
-    time_slices = slice_data(raw_df, start_time=start_time, interval=interval)
+    time_slices = slice_data(raw_df, start_time=start_time, end_time=end_time, interval=interval)
     line_data = pd.DataFrame.from_dict({time: record[content_field+"_sentiment"].mean() for time, record in time_slices.items()},
                                        orient="index")
     line_data = line_data.sort_index()
@@ -165,11 +167,11 @@ def data_to_areas(plot_data):
     lower_bound = lower_bounds.min()
     lower_bounds[negative_lower_bounds] /= lower_bound
     lower_bounds[negative_lower_bounds] *= sign
-    lower_bounds[lower_bounds>0] /= upper_bounds.max()
-    negative_upper_bounds=upper_bounds < 0
+    lower_bounds[lower_bounds > 0] /= upper_bounds.max()
+    negative_upper_bounds = upper_bounds < 0
     upper_bounds[negative_upper_bounds] /= lower_bound
     upper_bounds[negative_upper_bounds] *= sign
-    upper_bounds[upper_bounds>0] /= upper_bounds.max()
+    upper_bounds[upper_bounds > 0] /= upper_bounds.max()
 
     # concat the upper and lower bounds together - note that lower bounds is reversed.
     #    reversal draws it backwards - think of it as a polygon.  Reversing one draws from start to finish.
@@ -177,19 +179,21 @@ def data_to_areas(plot_data):
     time = np.hstack([plot_data.index[::-1], plot_data.index])
     return areas, time
 
-def sentiment_history_plot(plot_data):
+
+def sentiment_history_plot(plot_data, width=1900, height=850):
     areas, time = data_to_areas(plot_data["area"])
-    p = figure(x_axis_type="datetime")
+    p = figure(width=width, height=height, x_axis_type="datetime")
     colors = brewer["RdBu"][len(areas)][::-1]
+    renderers = {"area": {}}
     for index, cat in enumerate(reversed(plot_data["area"].columns)):
-        p.patch(time, areas[cat], color=colors[index], alpha=0.8, line_color=None, legend=cat)
-    p.line(plot_data["mean"].index, plot_data["mean"][0].values, legend="mean")
-    return p
+        renderers["area"][cat] = p.patch(time, areas[cat], color=colors[index], alpha=0.8, line_color=None, legend=cat)
+    renderers["mean"] = p.line(plot_data["mean"].index, plot_data["mean"][0].values, legend="mean")
+    return p, renderers
 
 
 def show_sentiment_history_plot(plot_data, filename="sentiment_history.png"):
     output_file(filename=filename)
-    show(sentiment_history_plot(plot_data))
+    show(sentiment_history_plot(plot_data)[0])
 
 
 # need a way to parse time intervals.  Gist from: